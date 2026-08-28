c683588fc64d38cc57ff3fb4dcb3acdadc7cd383
LeeJoonBin <dlwnsqls22@gmail.com>
2026-08-28 16:54:44 +0900

    ㅜ

--- 1차정규평가_이준빈/1차정규평가_이준빈/Program.cs
+++ 1차정규평가_이준빈/1차정규평가_이준빈/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace _1차정규평가_이준빈;
+﻿// 
+namespace _1차정규평가_이준빈;
 using System;
 using System.Collections.Generic;
 
 public class Program
 {
     public const string STORE_NAME = "정규시험 상점";
+
     static void Main(string[] args)
     {
         // 아이템 목록
@@ -27,9 +29,11 @@ public class Program
         Console.WriteLine("------------------------");
         for (int i = 0; i < shopItems.Count; i++)
         {
-            Console.WriteLine($"{i+1}번 {shopItems[i].Name} ({shopItems[i].ItemCategory}) {shopItems[i].Prise}골드  [{shopItems[i].Description}]");
-                    
-        } 
+            Console.WriteLine(
+                $"{i + 1}번 {shopItems[i].Name} ({shopItems[i].ItemCategory}) {shopItems[i].Prise}골드  [{shopItems[i].Description}]");
+
+        }
+
         Console.WriteLine("------------------------");
         Console.WriteLine("[장바구니]");
         Console.WriteLine();
@@ -41,55 +45,22 @@ public class Program
             currentTotal += price;
             Console.WriteLine($"{slot.Product.Name} x {slot.Quantity} = {price}골드");
         }
+
         Console.WriteLine($"합계 = {currentTotal}골드");
         Console.WriteLine("------------------------");
         int choiceMenu = ConsoleInput.ReadIntInRange("1. 메뉴 담기   2. 전체 비우기   3. 결제   4. 영업 종료 번호 : ", 1, 4);
 
-       
+
         switch (choiceMenu)
         {
             case 1:
-                
-                
+
+
                 int number = ConsoleInput.ReadIntInRange("아이템 번호: ", 1, shopItems.Count) - 1;
                 ConsoleInput.Pause();
                 int amount = ConsoleInput.ReadIntAtLeast("수량을 입력하세요:", 0);
                 invertory.Add(new CartItem(shopItems[number], amount));
                 break;
-            case 2:
-                Console.Clear();
-                ConsoleInput.Pause();
-                break;
-                
-            case 3:
-                if (currentTotal == 0)
-                {
-                    Console.WriteLine("장바구니가 비어있습니다.");
-                    break;
-                }
-
-                Console.WriteLine($"합계 급액: {currentTotal}골드");
-                int payment = ConsoleInput.ReadIntAtLeast("지불할 골드:", 0);
-
-                if (payment < currentTotal)
-                {
-                    Console.WriteLine("골드부족");
-                }
-                else
-                {
-                    Console.WriteLine($"결제완료 거스름돈 {payment - currentTotal}");
-                    totalGoldEarned += currentTotal;
-                    transactionCount++;
-                    invertory.Clear();
-                }
-                ConsoleInput.Pause();
-                break;
-            case 4:
-                Console.WriteLine("\n==================================");
-                Console.WriteLine($"총 거래 횟수: {transactionCount}회");
-                Console.WriteLine($"총 매출: {totalGoldEarned}G");
-                Console.WriteLine("상점 문을 닫습니다.");
-                return;
         }
     }
 }
