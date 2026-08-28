a5e077c5cde607de28455b40e22a04083921589c
LeeJoonBin <dlwnsqls22@gmail.com>
2026-08-28 10:35:49 +0900

    [Feat] 추상클래스(ItemMenu) 필드 및 생성자 입력

--- 1차정규평가_이준빈/1차정규평가_이준빈/ItemMenu.cs
+++ 1차정규평가_이준빈/1차정규평가_이준빈/ItemMenu.cs
@@ -2,5 +2,15 @@
 // 추상클래스 2 아이템메뉴
 public abstract class ItemMenu
 {
+    protected string Name;
+    protected string Description;
+    protected string Prise;
+
+    public ItemMenu(string name, string description, string prise)
+    {
+        Name = name;
+        Description = description;
+        Prise = prise;
+    }
     
 }
