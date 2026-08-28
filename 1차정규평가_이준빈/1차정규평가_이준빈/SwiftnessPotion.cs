a1b3a9fa8e94f998fcfc95bae03820686cda643a
LeeJoonBin <dlwnsqls22@gmail.com>
2026-08-28 10:33:11 +0900

    Program 클래스 지움

--- 1차정규평가_이준빈/1차정규평가_이준빈/SwiftnessPotion.cs
+++ 1차정규평가_이준빈/1차정규평가_이준빈/SwiftnessPotion.cs
@@ -1,5 +1,5 @@
 ﻿namespace _1차정규평가_이준빈;
-
+// 할인 가격 적용예정
 public class SwiftnessPotion : PotionMenu
 {
     
---
+++ 1차정규평가_이준빈/1차정규평가_이준빈/ThrowingWeapon.cs
@@ -0,0 +1,6 @@
+﻿namespace _1차정규평가_이준빈;
+// 할일 가격 적용 예정
+public class ThrowingWeapon : ItemMenu
+{
+    
+}
