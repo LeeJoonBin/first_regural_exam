eb94ed25b1ba1cce7da47e3584aa090717ff6172
LeeJoonBin <dlwnsqls22@gmail.com>
2026-08-28 10:25:35 +0900

    [Feat] PotionMenu의 파생 클래스(Health, Mana, Swiftness) 생성

--- 1차정규평가_이준빈/1차정규평가_이준빈/ItemMenu.cs
+++ 1차정규평가_이준빈/1차정규평가_이준빈/ItemMenu.cs
@@ -1,6 +1,6 @@
 ﻿namespace _1차정규평가_이준빈;
 // 추상클래스 2 아이템메뉴
-public class ItemMenu
+public abstract class ItemMenu
 {
     
 }
---
+++ 1차정규평가_이준빈/1차정규평가_이준빈/Manapotion.cs
@@ -0,0 +1,6 @@
+﻿namespace _1차정규평가_이준빈;
+
+public class Manapotion : PotionMenu
+{
+    
+}
